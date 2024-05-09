8a22dfbf97874ded8892871a9b7c0b74f49f8666
IProgram <107779614+imjusthereforminecraft@users.noreply.github.com>
2024-05-09 14:48:45 -0400

    Create blockify.lua

---
+++ blockify.lua
@@ -0,0 +1,38 @@
+local function send(file, id)
+    local file = fs.open(file, "r")
+    local data = file.readAll()
+    file.close()
+    rednet.send(id, {name = file, data = data})
+end
+
+local function receive()
+    local sender, data = rednet.receive()
+    local sender, name = rednet.receive()
+    local file = fs.open(mainname, "w")
+    file.write(data.data)
+    file.close()
+    return data.name
+end
+::loop::
+mainname = read()
+term.clear()
+term.setCursorPos(1,1)
+if mainname ~= "list" then
+    rednet.send(47, mainname)
+    receive()
+    if pocket then
+    pocket.equipBack()
+    end
+    shell.run("austream "..mainname.." streamData = true")
+    if pocket then
+    pocket.equipBack()
+    end
+else
+    rednet.send(47, mainname)
+    thing = ""
+end
+while thing ~= "end" do
+    sender, thing = rednet.receive()
+    print(thing)
+end
+goto loop
